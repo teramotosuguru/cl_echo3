ac052e4df89c3e2d90519f2297bb465e92ed33ff
wMETAw <wgn8404@gmail.com>
2014-02-25 22:02:02 +0900

    ビルダー作成

--- application/controllers/bigecho.php
+++ application/controllers/bigecho.php
@@ -7,8 +7,26 @@ class Bigecho extends CI_Controller {
 		parent::__construct();
 	}
 
-    public function index()
+    public function bigecho($text)
     {
+
+    	// 実行できないので動くかどうかは不明ですwww
+
+        $text = "aaa";
+
+        $this->load->library('text_builder');
+
+        // ビルダーを生成
+        $builder = new Text_Builder();
+
+        // テキストクラスを生成
+        $tex = $builder->build($text);
+
+        // 関数名をセット
+        $tex->set_param(__FUNCTION__);　// normalechoの場合はこいつを呼ばない
+
+        var_dump($tex->get_text());
+
     	$texts = array("a");
         $this->load->view('result', $texts);
     }
